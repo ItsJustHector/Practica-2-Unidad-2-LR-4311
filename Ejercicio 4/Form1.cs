8b7cc05b100a547747e41ef5b21f3fdb0cb03165
ItsJustHector <Hjimenezmorillo@gmail.com>
2025-06-27 22:50:05 -0400

    Subida del Ejercicio 4
    En este commit subo el  ejercicio 4

---
+++ Ejercicio 4/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EJERCICIO4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            string palabra = txtPalabra.Text.ToLower().Replace(" ", ""); // todo en minúscula y sin espacios
+
+            string palabraInvertida = new string(palabra.Reverse().ToArray());
+
+            if (palabra == palabraInvertida)
+            {
+                MessageBox.Show("¡Esta es una palabra palíndroma!", "Resultado");
+            }
+            else
+            {
+                MessageBox.Show("Esta no es una palabra palíndroma.", "Resultado");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
